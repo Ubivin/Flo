22545fa49bf8198558e9b3c1d32a11897f9c3ada
Ubivin <Ubivin@Jasons-MacBook-Pro.local>
2014-07-14 17:04:39 -0700

    Added Scroll Background

--- Source/MainScene.h
+++ Source/MainScene.h
@@ -8,6 +8,8 @@
 
 #import "CCNode.h"
 
+
 @interface MainScene : CCNode
+
 
 @end
--- Source/MainScene.m
+++ Source/MainScene.m
@@ -2,12 +2,57 @@
 //  MainScene.m
 //  PROJECTNAME
 //
-//  Created by Viktor on 10/10/13.
-//  Copyright (c) 2013 Apportable. All rights reserved.
+//  Created by Jason Hoang
+//
 //
 
 #import "MainScene.h"
+//scrolling speed IS THIS STILL NEEDED?!
+static const CGFloat scrollSpeed = 80.f;
+@implementation MainScene {
+    CCSprite *_green;
+    CCSprite *_red;
+    CCPhysicsNode *_physicsNode;
+    CCNode *_ground1;
+    CCNode *_ground2;
+    NSArray *_grounds;
+    CCNode *_leftSide;
+    CCNode *_rightSide;
+}
 
-@implementation MainScene
+
+-(void)didLoadFromCCB {
+    self.userInteractionEnabled = TRUE;
+    _grounds =@[_ground1,_ground2];
+      self.multipleTouchEnabled = YES;
+    
+}
+//// This moves the red and green with each touch
+//-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+//    [_green.physicsBody applyImpulse:ccp(0, 400.f)];
+//    [_red.physicsBody applyImpulse:ccp(0, 400.f)];
+//
+//}
+
+
+- (void)update:(CCTime)delta {
+    //positioning for the green and red thingy majig
+    _green.position = ccp(_green.position.x, _green.position.y - delta * scrollSpeed);
+    _red.position = ccp(_red.position.x, _red.position.y - delta * scrollSpeed);
+    //The direction of the background moving
+    _physicsNode.position = ccp(_physicsNode.position.x, _physicsNode.position.y  - (scrollSpeed *delta));
+    for (CCNode *ground in _grounds) {
+        ground.position = ccp(ground.position.x, ground.position.y - 1);
+            //Loops the two backgrounds endlessly
+                if (ground.position.y <= (-1 * (ground.contentSize.height*2))) {
+                    ground.position = ccp(ground.position.x, ground.position.y +
+                                              2 * (ground.contentSize.height*2));
+                }
+            }
+    }
+
+
+
+
 
 @end

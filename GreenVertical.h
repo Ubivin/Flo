a06e0f9b71c7bf73cad761c4bb66c117f57c3bf0
Ubivin <Ubivin@Jasons-MacBook-Pro-2.local>
2014-08-07 16:16:29 -0700

    pushpush

---
+++ GreenVertical.h
@@ -0,0 +1,16 @@
+//
+//  GreenVertical.h
+//  FlowJJ1
+//
+//  Created by Jason Hoang on 8/1/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GreenVertical : CCNode {
+    
+}
+
+@end
---
+++ GreenVertical.m
@@ -0,0 +1,14 @@
+//
+//  GreenVertical.m
+//  FlowJJ1
+//
+//  Created by Jason Hoang on 8/1/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import "GreenVertical.h"
+
+
+@implementation GreenVertical
+
+@end
